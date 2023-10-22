f142fd6ed311efa6365c1781f003cdc7e34ca6bc
hang <2283582431@qq.com>
2023-10-22 21:45:57 +0800

    '修改项目静态资源'

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import './assets/main.css'
-
+// scss 全局导入
+import './assets/styles/animate.scss'
+import './assets/styles/init.scss'
+import './assets/styles/index.scss'
+import 'element-plus/theme-chalk/src/message.scss'
+// unocss css
+import 'virtual:uno.css'
+// swiepr
+import 'swiper/css'
+import 'swiper/css/navigation'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -10,5 +19,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.mount('#app')
